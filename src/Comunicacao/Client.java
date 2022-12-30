9419eff6a7733a583cd64beb33cd070c060c364e
Eduardo Henriques <a93186@uminho.pt>
2022-12-30 15:53:01 +0000

    Working tests

--- src/Comunicacao/Client.java
+++ src/Comunicacao/Client.java
@@ -24,6 +24,7 @@ public class Client
     public static void main(String [] args) throws IOException {
         Socket server = new Socket("localhost",1584);
         Demultiplexer demultiplexer = new Demultiplexer(new TaggedConnection(server));
+        demultiplexer.start();
         
         int counter = 0;
         //MensagemAutenticacao m = new MensagemAutenticacao(0,"seila","seila");
@@ -32,37 +33,45 @@ public class Client
         //System.out.println("Pedido enviado");
         //taggedConnection.send(m.createFrame());
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        System.out.println("-----MENU------\nRESERVA-->1\nTROTINETES-->2\nRECOMPENSA-->3\nESTECIONAMENTO-->4");
+        System.out.println("-----MENU------\nTROTINETES-->1\nRECOMPENSAS-->2\nRESERVAR-->3\nESTACIONAMENTO-->4\nNOTIFICACOES-->5");
         while(true){
             String tipomsg = reader.readLine();
             if(tipomsg != null){
                 switch(tipomsg){
                     case "1":
-                        int x_reserva = Integer.parseInt(reader.readLine()); 
-                        int y_reserva = Integer.parseInt(reader.readLine());
-                        Mensagem mensagem_reserva  = new MensagemTrotinetes(counter, x_reserva, y_reserva,new String[1]);
-                        counter++;
-                        Thread atende_reserva = new Thread(new ProcessaPedido(demultiplexer, mensagem_reserva));
-                        atende_reserva.start();
-                        break;
-                    case "2":
-                        int x_trotinete = Integer.parseInt(reader.readLine()); 
-                        int y_trotinete = Integer.parseInt(reader.readLine());
-                        Mensagem mensagem_trotinete  = new MensagemTrotinetes(counter, x_trotinete, y_trotinete,new String[1]);
+                        System.out.println("insira o x:");
+                        int x_trotinete= Integer.parseInt(reader.readLine()); 
+                        System.out.println("insira o y:");
+                        int y_trotinete= Integer.parseInt(reader.readLine());
+                        Mensagem  mensagem_trotinete = new MensagemTrotinetes(counter, x_trotinete, y_trotinete,new String[1]);
                         counter++;
                         Thread atende_trotinete = new Thread(new ProcessaPedido(demultiplexer, mensagem_trotinete));
                         atende_trotinete.start();
                         break;
-                    case "3":
-                        int x_recompensa = Integer.parseInt(reader.readLine()); 
+                    case "2":
+                        System.out.println("insira o x:");
+                        int x_recompensa  = Integer.parseInt(reader.readLine()); 
+                        System.out.println("insira o y:");
                         int y_recompensa = Integer.parseInt(reader.readLine());
-                        Mensagem mensagem_recompensa  = new MensagemTrotinetes(counter, x_recompensa, y_recompensa,new String[1]);
+                        Mensagem mensagem_recompensa  = new MensagemRecompensas(counter, x_recompensa, y_recompensa,new String[1]);
                         counter++;
                         Thread atende_recompensa = new Thread(new ProcessaPedido(demultiplexer, mensagem_recompensa));
                         atende_recompensa.start();
                         break;
+                    case "3":
+                        System.out.println("insira o x:");
+                        int x_reservar = Integer.parseInt(reader.readLine()); 
+                        System.out.println("insira o y:");
+                        int y_reservar = Integer.parseInt(reader.readLine());
+                        Mensagem  mensagem_reserva = new MensagemReservar(counter, x_reservar, y_reservar,"");
+                        counter++;
+                        Thread atende_reserva = new Thread(new ProcessaPedido(demultiplexer, mensagem_reserva));
+                        atende_reserva.start();
+                        break;
                     case "4":
+                        System.out.println("insira o y:");
                         int x_estacionamento = Integer.parseInt(reader.readLine()); 
+                        System.out.println("insira o y:");
                         int y_estacionamento = Integer.parseInt(reader.readLine());
                         String codigo = reader.readLine();
                         Mensagem mensagem_estacionamento  = new MensagemEstacionamento(counter, x_estacionamento, y_estacionamento, codigo, "");
@@ -70,10 +79,21 @@ public class Client
                         Thread atende_estacionamento = new Thread(new ProcessaPedido(demultiplexer, mensagem_estacionamento));
                         atende_estacionamento.start();
                         break;
+                    case "5":
+                        System.out.println("insira o y:");
+                        int x_notifs = Integer.parseInt(reader.readLine()); 
+                        System.out.println("insira o y:");
+                        int y_notifs = Integer.parseInt(reader.readLine());
+                        Mensagem mensagem_notificacao  = new MensagemNotificacoes(counter, x_notifs, y_notifs);
+                        counter++;
+                        Thread atende_notificacao = new Thread(new ProcessaPedido(demultiplexer, mensagem_notificacao));
+                        atende_notificacao.start();
+                        break;
                     default:
                         System.out.println("escreva um valor correto");
                         break;
-                }      
+                } 
+                System.out.println("-----MENU------\nTROTINETES-->1\nRECOMPENSAS-->2\nRESERVAR-->3\nESTACIONAMENTO-->4\nNOTIFICACOES-->5");
             }else
                 break;
         }
